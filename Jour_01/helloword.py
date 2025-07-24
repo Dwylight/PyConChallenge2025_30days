2ce9b8f475b13acdfc65ae1da9e8fe4d7d62f782
Essoyoda Damienne BATCHATCHILE <batchatchiledada@gmail.com>
2025-07-24 00:00:15 +0000

    Create helloword.py

---
+++ Jour_01/helloword.py
@@ -0,0 +1,60 @@
+#Exercice 1
+
+python
+
+#Addition
+print(3 + 4)
+#subtraction(-)
+print(3 - 4)
+#multiplication(*)
+print(3 * 4)
+#modulus(%)
+print(3 % 4)
+#division(/)
+print(3 / 4)
+#exponential(**)
+print(3 ** 4)
+#floor division operator(//)
+print(3 // 4)
+
+#2
+# My name
+print("My name is Essoyoda Damienne")
+# My family name
+print("My family name is BATCHATCHILE")
+# My country
+print("My country is Togo")
+print("I am enjoying 30 days of python")
+
+#3 Type od data
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Your name"))
+print(type("Your family name"))
+print(type("Your country"))
+
+# Exercice 3
+'''1) Write an example for different Python data types such as Number(Integer, Float,
+Complex), String, Boolean, List, Tuple, Set and Dictionary.'''
+#String
+"I will be a great ingineer!!"
+#Boolean
+True
+#List
+["Mango", 24, "Ali"]
+#Tuple
+(12, 14, 9, 7)
+#Set
+{"Afi", "Fifi", 37, 68}
+#Dictionary
+{"Foufou": 4, "Riz": 13, "fetri": 19}
+
+#2
+from math import *
+point_1 = (2, 3)
+point_2 = (10, 8)
+euclidian_distance = sqrt((3-2)**2 + (8-10)**2)
+print(euclidian_distance)
